cb5c83b8ec32533b41f33d40a5884c80b6f848b1
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2021-05-25 11:13:06 +0200

    add model&migration (product)

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,6 +7,21 @@
     <title>navbar</title>
 </head>
 <body>
-    <h1>navbar</h1>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">WeFashion</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Soldes</a>
+              <a class="nav-link active" aria-current="page" href="#">Homme</a>
+              <a class="nav-link active" aria-current="page" href="#">Femme</a> 
+          </div>
+          </div>
+      </div>
+    </nav>
 </body>
 </html>
