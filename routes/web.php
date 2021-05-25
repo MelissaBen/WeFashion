d828a47eb2e9ec95883dc407a17008200a16c28d
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2021-05-25 10:45:52 +0200

    add MainController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/', [MainController::class , 'home'])->name('home');
+
