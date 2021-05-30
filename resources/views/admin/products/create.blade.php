8fd9db6bd7dfea05e8fde11312c7ea802547af03
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2021-05-30 09:05:53 +0200

    ADD changes

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -10,47 +10,55 @@
 <body>
     @extends('base')
     @section('content')
-       <section class="container " style="margin-bottom: 10%">
+       <section class="container card my-5" >
       
-            <h1>ici ajouter un produit</h1>
+            <h2 class="text-center mt-5"> Ajouter un produit</h2>
             <form method="POST" action="{{route('products.store')}}">
               @csrf
-              <div class="col-12">
-                <div class="form-group">
+              <div class="col-12 m-5">
+                <div class="form-group m-5 ">
                   <label for="">nom</label>
                   <input type="text" name='title' class="form-control" placeholder="nom du produit">
                 </div>
 
-                <div class="form-group">
-                  <label for="exampleFormControlTextarea1" class="form-label">description</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
+                <div class="form-group m-5 ">
+                  <label for="">Catégorie</label>
+                  <input type="text" name='title' class="form-control" placeholder="nom de la catégorie">
+                  
                 </div>
                 
-                <div class="form-group">
+
+                <div class="form-group m-5">
+                  <label for="exampleFormControlTextarea1" class="form-label">description</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" placeholder="description"></textarea>
+                </div>
+                
+                
+                <div class="form-group m-5">
                     <label for="price">Prix</label>
-                    <input type="number" class="form-control" id="price" name="price" step="0.01" min="0.01" max="9999.99" value="" required>
+                    <input type="number" class="form-control" id="price" name="price" step="0.01" min="0.01" max="9999.99" value="" placeholder="prix" required>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group m-5">
                   <label for="reference">Référence</label>
-                  <input type="text" minlength="16" maxlength="16" class="form-control" id="reference" name="reference" value="" required>
+                  <input type="text" minlength="16" maxlength="16" class="form-control" id="reference" name="reference" value="" placeholder="référence" required>
                 </div>
 
             
               
-                <div class="form-group">
+                <div class="form-group m-5">
                     <label>En solde</label> <br>
-                    <div class="form-check form-check-inline">
+                    <div class="form-check form-check-inline m-2">
                         <input class="form-check-input" type="radio" id="discount0" name="discount" value="1" checked >
                         <label class="form-check-label" for="discount0">Oui</label>
                     </div>
-                    <div class="form-check form-check-inline">
+                    <div class="form-check form-check-inline m-2">
                         <input class="form-check-input" type="radio" id="discount1" name="discount" value="0"  checked>
                         <label class="form-check-label" for="discount1">Non</label>
                     </div>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group m-5">
                     <label for="picture">Photo</label>
                     <input type="text" class="form-control-file" name="image" id="picture">
                 </div>
